ff534146d22f06dbfeff50f2757e40cd8ddbff56
John Retsas <joretsas@gmail.com>
2019-06-05 17:53:51 +0300

    Simple Blog

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def index(request):
-	# return HttpResponse('Hello From Posts')
 
-	return render(request, 'posts/index.html', {
-		'title': 'Latest Posts'
-	})
+from .models import Posts
+# Create your views here.
+
+
+def index(request):
+        # return HttpResponse('Hello From Posts')
+
+
+	posts = Posts.objects.all()[:10]
+
+
+	context = {
+		'title': 'Latest Posts',
+		'posts': posts
+		
+	}
+	return render(request, 'posts/index.html', context)
+
+def details(request, id):
+	post = Posts.objects.get(id = id)
+
+	context = {
+		'post': post
+	}
+	return render(request, 'posts/details.html', context)
